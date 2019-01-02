e4028daa4370a9189746cc553f57d8040bb2149a
Alberto Schena <alberto.schena@addvalue.it>
2019-01-02 18:09:45 +0100

    inizio studio hotswap versione servizio forex

---
+++ forex-service-v2/src/main/java/com/arca/microservices/forex/service/ExchangeValue.java
@@ -0,0 +1,23 @@
+package com.arca.microservices.forex.service;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+public
+class ExchangeValue
+{
+	@Id
+	private Long       id;
+	@Column(name = "currency_from")
+	private String     from;
+	@Column(name = "currency_to")
+	private String     to;
+	private BigDecimal conversionMultiple;
+	private int        port;
+}
