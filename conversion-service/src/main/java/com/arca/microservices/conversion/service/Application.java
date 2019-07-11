773c82820e612c87386b35e203652fb5f64286d4
Alberto Schena <alberto.schena@addvalue.it>
2019-07-11 18:38:34 +0200

    retry

--- conversion-service/src/main/java/com/arca/microservices/conversion/service/Application.java
+++ conversion-service/src/main/java/com/arca/microservices/conversion/service/Application.java
@@ -3,11 +3,16 @@ package com.arca.microservices.conversion.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableFeignClients(basePackageClasses = Application.class)
 @EnableDiscoveryClient
+@EnableRetry
+@Import(RibbonAutoConfiguration.class)
 public
 class Application
 {
--- zuul/src/main/java/com/arca/microservices/zuul/Application.java
+++ zuul/src/main/java/com/arca/microservices/zuul/Application.java
@@ -3,15 +3,20 @@ package com.arca.microservices.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonAutoConfiguration;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+@SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
-@SpringBootApplication
+@EnableRetry
+@Import(RibbonAutoConfiguration.class)
 public
 class Application
 {
